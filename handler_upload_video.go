f57875e975f441bdda54b59fcb59af3b1b37ea9b
valentinvisan <valentin.visan.vv@gmail.com>
2025-04-20 09:00:14 +0300

    feature :: wrap up course

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -51,13 +51,13 @@ func (cfg apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	url, err := cfg.uploadVideoToS3(r.Context(), tempFile, mediaType, filename)
-	if err != nil {
+	if err = cfg.uploadVideoToS3(r.Context(), tempFile, mediaType, filename); err != nil {
 		respondWithError(w, http.StatusInternalServerError, unableToUploadVideo, err)
 		return
 	}
 
-	video.VideoURL = &url
+	videoUrl := cfg.s3CfDistribution + "/" + filename
+	video.VideoURL = &videoUrl
 	if err = cfg.db.UpdateVideo(video); err != nil {
 		respondWithError(w, http.StatusInternalServerError, unableToUploadVideo, err)
 		return
@@ -105,33 +105,27 @@ func saveTempFile(src multipart.File) (*os.File, error) {
 	}
 
 	fastStartVideoFile, err := processVideoForFastStart(tempFile.Name())
+	closer(tempFile)
+	remover(tempFile.Name())
+
 	if err != nil {
-		closer(tempFile)
-		remover(tempFile.Name())
 		return nil, err
 	}
 
 	processedFile, err := os.Open(fastStartVideoFile)
 	if err != nil {
-		closer(tempFile)
-		remover(tempFile.Name())
+		remover(fastStartVideoFile)
 		return nil, err
 	}
-
-	defer remover(tempFile.Name())
-	defer remover(fastStartVideoFile)
-	defer closer(tempFile)
-	defer closer(processedFile)
 
 	_, err = processedFile.Seek(0, io.SeekStart)
 	if err != nil {
-		closer(tempFile)
-		remover(tempFile.Name())
 		closer(processedFile)
-		remover(processedFile.Name())
+		remover(fastStartVideoFile)
 		return nil, err
 	}
 
+	// Let the caller defer these
 	return processedFile, nil
 }
 
@@ -144,7 +138,7 @@ func generateVideoFilename(orientation string) (string, error) {
 	return fmt.Sprintf("%s/%s.mp4", orientation, base64.RawURLEncoding.EncodeToString(bytes)), nil
 }
 
-func (cfg apiConfig) uploadVideoToS3(ctx context.Context, file *os.File, mediaType, filename string) (string, error) {
+func (cfg apiConfig) uploadVideoToS3(ctx context.Context, file *os.File, mediaType, filename string) error {
 	_, err := cfg.s3Client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
 		Key:         &filename,
@@ -152,10 +146,10 @@ func (cfg apiConfig) uploadVideoToS3(ctx context.Context, file *os.File, mediaTy
 		ContentType: &mediaType,
 	})
 	if err != nil {
-		return "", err
+		return err
 	}
-	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, filename)
-	return url, nil
+
+	return nil
 }
 
 func (cfg apiConfig) retrieveAndValidateVideo(w http.ResponseWriter, r *http.Request) (error, database.Video, bool) {
